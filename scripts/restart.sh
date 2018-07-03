344033a58ce8303cb51af20f49483ef0e99f8a19
nakabonne <rodriguez.nak@gmail.com>
2018-07-03 18:25:43 +0900

    add build script to restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -3,6 +3,8 @@ set -e
 cd /home/isucon/isubata
 git pull
 sudo cp etc/nginx.conf /etc/nginx/nginx.conf
+cd /home/isucon/isubata/webapp/go
+make
 sudo /usr/sbin/nginx -t
 sudo service nginx reload
 sudo service isubata.golang restart

3e638f81e37ef1c21b5149703c055440b57b1427
nakabonne <rodriguez.nak@gmail.com>
2018-07-03 11:15:13 +0900

    add indexes

--- sql/index.sql
+++ sql/index.sql
@@ -1,1 +1,5 @@
 create index index_message_on_channel_id_and_id on message (channel_id, id);
+alter table user add index (name);
+alter table message add index(channel_id);
+alter table image add index(name);
+alter table channel add index (message_count);
